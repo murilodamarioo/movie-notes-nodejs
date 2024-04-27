12966f3bdbed25c69d16bf7aac4abff862e4eb16
Murilo Damario <murilo.gcdamario@gmail.com>
2024-04-27 14:55:06 -0300

    Implement AppError

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,9 +1,11 @@
+const AppError = require('../utils/AppError')
+
 class UsersController {
 
     create(request, response) {
         const { name, email, password } = request.body
 
-        response.json({ name, email, password })
+        response.status(201).json({ name, email, password })
     }
 }
 

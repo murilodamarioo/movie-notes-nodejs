42ffad410c626b06f920ab14cae9f0f369589a29
Murilo Damario <murilo.gcdamario@gmail.com>
2024-04-27 22:29:42 -0300

    Configuration where migrations will be created

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,11 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve(__dirname, "src", "database", "database.db")
+      filename: path.resolve(__dirname, 'src', 'database', 'database.db')
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
+    migrations: {
+      directory: path.resolve(__dirname, 'src', 'database', 'knex', 'migrations')
+    }
   }
 };

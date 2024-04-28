9f557e14a9e6aa7330351d1903594d656a594827
Murilo Damario <murilo.gcdamario@gmail.com>
2024-04-28 00:00:03 -0300

    Refactor UsersController and implement bcrypt lib

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,11 +1,32 @@
+const { hash, compare } = require("bcryptjs")
+
 const AppError = require('../utils/AppError')
+
+const knex = require('../database/knex')
 
 class UsersController {
 
-    create(request, response) {
+    async create(request, response) {
+        // First extract some infos from request body
         const { name, email, password } = request.body
+        
+        const [checkUserExists] = await knex('users').select(['email']).where('email', email)
+        console.log(checkUserExists)
 
-        response.status(201).json({ name, email, password })
+        if (checkUserExists) {
+            throw new AppError('Este e-mail já está cadastrado')
+        }
+        console.log(checkUserExists)
+
+        const hashPassword = await hash(password, 8)
+
+        await knex('users').insert({
+            name,
+            email,
+            password: hashPassword
+        })
+
+        response.status(201).json()
     }
 }
 

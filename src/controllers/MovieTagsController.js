e66b0c46d237645e1f62d72b0d18a85dae6de5d7
Murilo Damario <murilo.gcdamario@gmail.com>
2024-04-28 23:28:10 -0300

    Implement MovieTagsController

--- src/controllers/MovieTagsController.js
+++ src/controllers/MovieTagsController.js
@@ -5,7 +5,11 @@ const knex = require('../database/knex')
 class MovieTagsController {
     
     async index(request, response) {
+        const { user_id } = request.query
 
+        const tags = await knex('movie_tags').where({ user_id })
+
+        return response.status(200).json(tags)
     }
 }
 
